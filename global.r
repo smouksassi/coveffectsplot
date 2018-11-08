34bd5edbacdeafd8c4a2fcdb0050515a3be6dc87
Dean Attali <daattali@gmail.com>
2018-11-08 12:08:15 -0500

    small cleanup

--- global.r
+++ global.r
@@ -1,20 +1,23 @@
-suppressPackageStartupMessages(library(dplyr)) 
-suppressPackageStartupMessages(library(tidyr))
-suppressPackageStartupMessages(library(egg)) 
-suppressPackageStartupMessages(library(ggplot2))
-suppressPackageStartupMessages(library(ggstance)) 
-suppressPackageStartupMessages(library(shiny))
-suppressPackageStartupMessages(library(shinyjs)) 
-suppressPackageStartupMessages(library(markdown)) 
-suppressPackageStartupMessages(library(colourpicker)) 
+suppressPackageStartupMessages({
+  library(dplyr)
+  library(tidyr)
+  library(egg) 
+  library(ggplot2)
+  library(ggstance)
+  library(shiny)
+  library(shinyjs) 
+  library(markdown)
+  library(colourpicker)
+})
 
-
-signif_pad <- function(x, digits=3, round.integers=TRUE, round5up=TRUE) {
+signif_pad <- function(x, digits = 3, round.integers = TRUE, round5up = TRUE) {
   eps <- ifelse(round5up, x*(10^(-(digits + 3))), 0)
   if (round.integers) {
-    cx <- as.character(signif(x+eps, digits))  # Character representation of x
+    cx <- as.character(signif(x + eps, digits))  # Character representation of x
   } else {
-    cx <- ifelse(x >= 10^digits, as.character(round(x)), as.character(signif(x+eps, digits)))  # Character representation of x
+    cx <- ifelse(x >= 10^digits,
+                 as.character(round(x)),
+                 as.character(signif(x + eps, digits)))  # Character representation of x
   }
   
   cx[is.na(x)] <- "0"                    # Put in a dummy value for missing x
@@ -22,12 +25,12 @@ signif_pad <- function(x, digits=3, round.integers=TRUE, round5up=TRUE) {
   d <- gsub("[^0-9]", "", cx)            # The 'digits' of x
   d <- sub("^0*", "", d)                 # Remove any leading zeros
   nd <- nchar(d)                         # How many actual digits
-  nd[cx=="0"] <- 1                       # Special case "0"
+  nd[cx == "0"] <- 1                     # Special case "0"
   npad <- pmax(0, digits - nd)           # How many digits are missing
-  pad <- sapply(npad, function(n) paste(rep("0", times=n), collapse=""))
+  pad <- sapply(npad, function(n) paste(rep("0", times = n), collapse = ""))
   
   has.dec <- grepl("\\.", cx)                      #  Does cx already contain a decimal point?
   add.dec <- ifelse(!has.dec & npad > 0, ".", "")  #  If not, and if padding is required, we need to add a decimal point first
   
-  ifelse(is.na(x), NA, paste(cx, add.dec, pad, sep=""))
+  ifelse(is.na(x), NA, paste(cx, add.dec, pad, sep = ""))
 }
